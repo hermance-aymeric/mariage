9d66409cfdd1743d55f594c7979d910b4d017e21
Dornier Hermance <hermance.dornier@SQ-MAC004.local>
2024-02-06 19:15:00 +0100

    adding logo

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import logo from "./../assets/logo.png";
+
 const Error: React.FC = () => {
-  return <div>Error</div>;
+  return (
+    <div>
+      <h1>404</h1>
+      <img src={logo} alt="logo" width={400} height={400} />
+    </div>
+  );
 };
 
 export default Error;
--- src/pages/Maps.tsx
+++ src/pages/Maps.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Button from "@mui/material/Button";
 
 const Maps: React.FC = () => {
   return (
     <div>
       <h1>Maps</h1>
-      <Button variant="contained">Hello world</Button>
     </div>
   );
 };
