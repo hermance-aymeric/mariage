15bb17bb4332d65dc545713d01b0d05bc6ce5272
Dornier Hermance <hermance.dornier@SQ-MAC004.local>
2024-02-06 17:22:56 +0100

    adding typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World
+          Hello World !
         </a>
       </header>
     </div>
