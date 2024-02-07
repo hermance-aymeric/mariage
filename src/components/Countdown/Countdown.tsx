069adceb75cd165f89d617301875b5643065eb1c
Dornier Hermance <hermance.dornier@SQ-MAC004.local>
2024-02-07 20:29:56 +0100

    home

--- src/components/Countdown/Countdown.tsx
+++ src/components/Countdown/Countdown.tsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from "react";
-import './Countdown.css'
+import styled from "styled-components";
+import "./Countdown.css";
 
 let targetTime = new Date("2024-09-14").getTime();
 // Add 15 hours because we're getting married on the 14th at  <3
-targetTime += (15 * 60 * 60 * 1000);
+targetTime += 15 * 60 * 60 * 1000;
 
 const Countdown: React.FC = () => {
   const [currentTime, setCurrentTime] = useState<number>(Date.now());
 
   const timeBetween: number = targetTime - currentTime;
-  const seconds:number = Math.floor((timeBetween / 1000) % 60);
-  const minutes:number = Math.floor((timeBetween / 1000 / 60) % 60);
-  const hours:number = Math.floor(((timeBetween / (1000 * 60 * 60))) % 24);
-  const days:number = Math.floor(timeBetween / (1000 * 60 * 60 * 24));
+  const seconds: number = Math.floor((timeBetween / 1000) % 60);
+  const minutes: number = Math.floor((timeBetween / 1000 / 60) % 60);
+  const hours: number = Math.floor((timeBetween / (1000 * 60 * 60)) % 24);
+  const days: number = Math.floor(timeBetween / (1000 * 60 * 60 * 24));
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -21,18 +22,33 @@ const Countdown: React.FC = () => {
 
     return () => clearInterval(interval);
   }, []);
-  console.log(Intl.DateTimeFormat().resolvedOptions().timeZone)
+  console.log(Intl.DateTimeFormat().resolvedOptions().timeZone);
 
   return (
-    <>
-      <h1 className="position-absolute top-50 start-50 translate-middle text-center text-black countdown-font">
-        <span>{days}Jours </span>
-        <span>{hours}heures </span>
-        <span>{minutes}minutes </span>
-        <span>{seconds}secondes</span>
-      </h1>
-    </>
+    <Container>
+      <CountdownText className="position-absolute translate-middle text-center">
+        <span>{days} Jours </span><br/>
+        <span>{hours} Heures </span><br/>
+        <span>{minutes} Minutes </span><br/>
+        <span>{seconds} Secondes</span><br/>
+      </CountdownText>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  font-family: Apple Chancery, cursive;
+  @media (max-width: 1024px) {
+  }
+`;
+const CountdownText = styled.h1`
+  color: black;
+  top: 50%;
+  left: 50%;
+  @media (max-width: 1024px) {
+    color: white;
+    top: 35%;
+  }
+`;
 
 export default Countdown;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,38 @@
 import React from "react";
 import Header from "../components/Header.tsx";
+import styled from "styled-components";
 import { ActiveNavItem } from "../type.ts";
 import FluidImage from "../components/FluidImage.tsx";
-import sunset from "./../assets/sunset-black-and-white.JPEG"
+import sunset from "./../assets/sunset-black-and-white.JPEG";
 import Countdown from "../components/Countdown/Countdown.tsx";
 
 const Home: React.FC = () => {
   return (
     <>
-      <Header activeItem={ActiveNavItem.Home}/>
-      <FluidImage src={sunset}/>
-      <Countdown/>
+      <Header activeItem={ActiveNavItem.Home} />
+      <FluidImage src={sunset} />
+      <Countdown />
+      <Text>
+        Nous sommes très impatients de vous recevoir afin de célébrer avec vous
+        notre mariage, le 14 Septembre 2024
+      </Text>
+      <Signature>Hermance & Aymeric</Signature>
     </>
   );
 };
 
+const Text = styled.div`
+  margin-top: 50px;
+  text-align: center;
+  font-family: Apple Chancery, cursive;
+  @media (max-width: 1024px) {
+  }
+`;
+const Signature = styled(Text)`
+  font-family: Apple Chancery, cursive;
+  @media (max-width: 1024px) {
+    color: #87a15b;
+  }
+  font-size: 28px;
+`;
 export default Home;
