1a3d830d4b254b627d568085ace488bb0c2058ea
Aymeric Larvet <aymeric.larvet@hubvisory.com>
2024-03-16 12:51:01 +0100

    :sparkles: Add mobile version

--- src/components/Gift/GiftSection.tsx
+++ src/components/Gift/GiftSection.tsx
@@ -4,18 +4,31 @@ import PhotosStack from "../PhotosStack/PhotosStack.tsx";
 import chalet from "./../../assets/chalet.png";
 import babyBed from "./../../assets/baby-bed.png";
 import Gift from "./Gift.tsx";
+import useIsMobile from "../../hook/useMobileSize.tsx";
 
 const GiftSection = () => {
+  const isMobile = useIsMobile();
   return (
     <Container fluid className="py-4 px-5 section-container">
-      <Row>
-        <Col>
-          <Gift />
-        </Col>
-        <Col>
-          <PhotosStack photos={[chalet, babyBed]} gap={"xs"} />
-        </Col>
-      </Row>
+      {isMobile ? (
+        <Row>
+          <Col>
+            <PhotosStack photos={[chalet, babyBed]} gap={"xs"} />
+          </Col>
+          <Col>
+            <Gift />
+          </Col>
+        </Row>
+      ) : (
+        <Row>
+          <Col>
+            <Gift />
+          </Col>
+          <Col>
+            <PhotosStack photos={[chalet, babyBed]} gap={"xs"} />
+          </Col>
+        </Row>
+      )}
     </Container>
   );
 };
